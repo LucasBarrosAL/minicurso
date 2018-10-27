8809f57e5eff464798edc0f92cc0f2883c58aaa4
Aluno <lucasbarros.al@gmail.com>
2018-10-27 14:44:04 -0300

    First

---
+++ app/src/main/java/com/example/lucasbarros/moviesminicurso/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.example.lucasbarros.moviesminicurso;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.lucasbarros.moviesminicurso.databinding.ActivityMain2Binding;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2);
+        setContentView(R.layout.activity_main2);
+
+//        EditText editText = (EditText) findViewById(R.id.email);
+        binding.email.setText("texttstasdasdasd asd ajkl sdasd");
+        binding.setAct(this);
+        binding.setLabel("IFSC");
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void loginClick(View view) {
+
+    }
+
+    public void loginClick() {
+        String email = binding.email.getText().toString();
+        String password = binding.password.getText().toString();
+        
+    }
+}
